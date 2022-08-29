323555f03da4f234efea7f62cb6682f16cff4a01
Wagner <wagnerghedin10@gmail.com>
2022-08-29 18:40:38 -0300

    Criando algoritmo

--- main.js
+++ main.js
@@ -1,51 +1,38 @@
 let gramatica1 = "aSb|ab"
+let derivacao = getDerivacao(gramatica1.split('|'));
 
-// console.log(getDerivacao(cadeia1));
-console.log(getDerivacao(gramatica1.split('|')));
-
-
-// function getDerivacao(cadeia) {
-//     let producoes = cadeia.split('|');
-//     let derivacoes = [];
-//     let selecao = producoes[0]
-//     let index = 0;
-//     while (producoes.size > 0) {
-//         selecao.forEach(char => {
-//             if (char != char.toUpperCase()) {
-//                 derivacoes.push(char);
-//                 // terminal
-//             } else {
-
-//                 // não terminal
-//             }
-//         });
-//     }
-//     return derivacoes;
-// }
+console.log(derivacao);
 
 function getDerivacao(gramatica) {
     let pilha = [gramatica[0]];
     let saida = [];
+    let cadeia = pilha.split('');
     while (pilha.length > 0) {
-        let cadeia = pilha.shift();
-        cadeia = cadeia.split('')
+        // let cadeia = cadeias.shift();
+        // cadeia = cadeia.split('')
         cadeia.forEach(char => {
             saida.push(char);
             if (char != char.toUpperCase()) {
                 // terminal
-                // saida.push(char);
+                saida.push(char);
             } else {
                 // não terminal
                 gramatica.forEach(cadeia => {
                     if (cadeia.includes(char)) {
-                        pilha.push(cadeia);
+                        // pilha.push(cadeia);
                     }
                 });
             }
         });
 
-        let random = Math.random(0, gramatica.length - 1)
+        let random = getRandomInt(0, gramatica.length - 1);
         pilha.push(gramatica[random])
     }
     return saida;
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
 }
